705b3ec07238d73170691cafe645fbaf1769a11a
lvtdien2001 <lvtdien.ag@gmail.com>
2023-11-07 15:17:57 +0700

    fix bug

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,84 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import UserUtteranceReverted
+from rasa_sdk.executor import CollectingDispatcher
+import pymongo
 #
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
 #
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["lvtn_2023"]
+products = mydb["products"]
+
+class ActionDefaultFallback(Action):
+    def name(sefl) -> Text:
+        return "action_default_fallback"
+    
+    async def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_default")
+
+        # Revert user message which led to fallback.
+        return [UserUtteranceReverted()]
+
+class ActionMaleWatch(Action):
+    def name(self) -> Text:
+        return "action_male_watch"
+
+    def run(
+        self, 
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+        quantity = products.count_documents({"gender": 1})
+        dispatcher.utter_message(
+            template='utter_male_watch',
+            quantity=quantity
+        )
+
+        return [UserUtteranceReverted()]
+
+class ActionFemaleWatch(Action):
+    def name(self) -> Text:
+        return "action_female_watch"
+
+    def run(
+        self, 
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+        quantity = products.count_documents({"gender": 2})
+        dispatcher.utter_message(
+            template='utter_female_watch',
+            quantity=quantity
+        )
+
+        return [UserUtteranceReverted()]
+
+class ActionCoupleWatch(Action):
+    def name(self) -> Text:
+        return "action_couple_watch"
+
+    def run(
+        self, 
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+        quantity = products.count_documents({"gender": 0})
+        dispatcher.utter_message(
+            template='utter_couple_watch',
+            quantity=quantity
+        )
+
+        return [UserUtteranceReverted()]
